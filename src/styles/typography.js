d695d3ac71079728b5c941b4e90d9068125c5c19
Sebastian Bustamante <sbustamante301@gmail.com>
2023-01-09 16:46:14 -0500

    Add fonts and typography

--- src/styles/typography.js
+++ src/styles/typography.js
@@ -35,43 +35,60 @@ export const fonts = {
       `,
     },
     head: {
+      xxxs: `
+      font-size: 0.875rem;
+      line-height: 1.25rem;
+      font-weight: 500;
+      `,
+      xxs: `
+      font-size: 1rem;
+      line-height: 1.5rem;
+      font-weight: 400;
+      `,
       xs: `
       font-size: 1.25rem;
       line-height: 1.75rem;
+      font-weight: 500;
       `,
       sm: `
       font-size: 1.5rem;
       line-height: 2rem;
+      font-weight: 400;
       `,
       md: `
       font-size: 2.25rem;
       line-height: 3rem;
+      font-weight: 400;
       `,
       lg: `
       font-size: 3rem;
       line-height: 4rem;
+      font-weight: 400;
       `,
       xl: `
       font-size: 4rem;
       line-height: 5.5rem;
+      font-weight: 300;
       `,
       xxl: `
       font-size: 6rem;
       line-height: 8rem;
+      font-weight: 300;
       `,
     },
   };
   
   for (const size in typography.text) {
     typography.text[size] += `
-    font-family: ${fonts.primary};
+    font-family: ${fonts.secondary};
+    
     `;
   }
   
   for (const size in typography.head) {
     typography.head[size] += `
-    font-family: ${fonts.secondary};
-    font-weight: 600;
+    font-family: ${fonts.primary};
+    
     `;
   }
   
