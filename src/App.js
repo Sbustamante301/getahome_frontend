bffb0abf1b8d8f37ff377fcfaec9d77e73d480f8
Olenka Sanchez <olesa600@gmail.com>
2023-01-10 16:47:25 -0500

    ADD: Input styles

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import { PrimaryButton } from "./components/Button";
 // gap: 26px;
 
 
-const Section1 = styled.div `
+const Section1 = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -24,10 +24,13 @@ const Section1 = styled.div `
   top: 1588px;
 `
 const TitleContainer = styled.div`
+  margin-top: 60px;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  gap:8px;
+  color:${colors.gray.dark}
 `
 
 const FiltersContainer = styled.div`
@@ -36,20 +39,43 @@ const FiltersContainer = styled.div`
   align-items: center;
   padding: 8px 16px;
   gap: 8px;
+  margin-top:64px;
+  boder: 1px solid black;
 
-  position: absolute;
   width: 800px;
   height: 72px;
   left: 320px;
   top: 252px;
 
-  /* White */
-
-  background: #FFFFFF;
-  /* Elevation1 */
+  background: ${colors.white};
 
   box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.2);
   border-radius: 8px;
+`;
+
+const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 0px;
+  gap: 8px;
+
+  width: 160px;
+  height: 56px;
+`;
+
+const InputLabel = styled.label`
+  ${typography.text.xxs};
+  color: ${colors.gray.medium}
+`;
+
+const Select = styled.select`
+  display: flex;
+  justify-content: strech;
+  width: 100%;
+  ${typography.text.md};
+  color: ${colors.gray.dark}
+  outline: none;
 `;
 
 
@@ -70,7 +96,7 @@ background: ${colors.pink.shallow}
 
 
 `;
-const Section4 = styled.div `
+const Section4 = styled.div`
 display: flex;
 flex-direction: column;
 align-items: center;
@@ -87,10 +113,12 @@ background: ${colors.white}`
 const Wrapper = styled.div``;
 
 const Section1Title = styled.h1`
+  margin: 0px;
   ${typography.head.xl};
 `;
 
 const Section1Subtitle = styled.h4`
+  margin: 0px;
   ${typography.head.sm};
 `;
 
@@ -114,20 +142,33 @@ flex-direction:column;
 function App() {
   return (
     <Wrapper>
-      <Section1 style={{ backgroundImage:`url(${buildings})`, width: '100%'}}>
+      <Section1 style={{ backgroundImage: `url(${buildings})`, backgroundPosition: 'center' }}>
         <TitleContainer>
           <Section1Title> Meet your new home</Section1Title>
           <Section1Subtitle>The easiest way to find where you belong</Section1Subtitle>
         </TitleContainer>
+        <FiltersContainer>
+          <InputContainer>
+            <InputLabel for="lookType">I'M LOOKING FOR</InputLabel>
+
+            <Select name="types" id="lookType">
+              <option value="apartment">An Apartment</option>
+              <option value="house">A House</option>
+            </Select>
+          </InputContainer>
+          <div></div>
+          <div></div>
+          <div></div>
+        </FiltersContainer>
       </Section1>
-     <Section3>
-      <Section3Title>Getting someone to rent your apartment has never been this easy
+      <Section3>
+        <Section3Title>Getting someone to rent your apartment has never been this easy
       <CreateAccountButton>Create An Account Now</CreateAccountButton>
-      </Section3Title>
-       
-       
-       
-     </Section3>
+        </Section3Title>
+
+
+
+      </Section3>
     </Wrapper>
 
   );
