2c7864c073a0b3a662bc47859fa3278e4a0dbf92
Jesus Barboza <jbarz20@gmail.com>
2023-01-13 10:57:58 -0500

    ADD:set margin and flex wrap to the filters

--- src/pages/home-page.js
+++ src/pages/home-page.js
@@ -45,6 +45,7 @@ const PropertyContainer = styled.div`
 
 function HomePage() {
   const { userType, properties, setSavedProperty, setUser } = useAuth();
+  console.log(properties)
   const randomProperties = properties.filter((_property, index) => index < 3)
   useEffect(() => {
     if(userType==="seeker"){
