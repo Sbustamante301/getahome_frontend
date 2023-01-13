cd7f33d7f68604633416152c7b47f8205d970ac3
Jesus Barboza <jbarz20@gmail.com>
2023-01-13 11:31:54 -0500

    update the user and usertype when a new user signUp

--- src/pages/properties-page.js
+++ src/pages/properties-page.js
@@ -13,14 +13,11 @@ import { getSaved } from "../services/properties-service";
 const ContainerList = styled.div`
 display: grid;
 grid-template-columns: 1fr 1fr 1fr;
-padding: 132px 184px 132px 184px;
+padding: 100px 184px;
 row-gap: 32px;
 column-gap: 86px;
 justify-items:center;
 justify-content:center;
-
-// overflow-y: scroll;
-// overflow-x:hidden;
 `
 const Wrapper = styled.div`
 padding: 32px 152px;
