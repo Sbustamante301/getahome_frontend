e62ac67b34fa9f4e7b4d34b3248a1f1d18d49123
Jesus Barboza <jbarz20@gmail.com>
2023-01-19 10:58:32 -0500

    ADD: .

--- src/components/sections/image-carousel.js
+++ src/components/sections/image-carousel.js
@@ -7,80 +7,69 @@ import house1 from "../../assets/test-images/house1.jpg"
 import house2 from "../../assets/test-images/house2.jpg"
 import house3 from "../../assets/test-images/house3.jpg"
 import styled from "@emotion/styled";
-import { useState,useEffect } from "react";
-
-
-
+import { useState } from "react";
 
 const StyledDiv = styled.div`
 display:flex;
 flex-direction:column;
 justify-content:center;
-
 `;
-
 const StyledDiv2 = styled.div`
 color:black;
-
 `;
 
-const SimpleSlider = () => {
-    const CustomPrevArrow = (props) => {
-        const { className, style, onClick } = props;
-        return (
-            <div
-                className={className}
-                style={{ ...style,background:"gray",height:"0px",width:"0px"}}
-                onClick={onClick}
-            >
-                {Icons.arrowLeft}
-            </div>
-        );
-    };
-    
-    const CustomNextArrow = (props) => {
-        const { className, style, onClick } = props;
-        return (
-            <div
-                className={className}
-                style={{ ...style,}}
-                onClick={onClick}>
-                {Icons.arrowRight}
-            </div>
-        );
-    };
-    const [settings, setSettings] = useState({
-      dots: false,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      fade: true,
-      arrows:true,
-      
-      prevArrow: <CustomPrevArrow />,
-      nextArrow: <CustomNextArrow />,
-    });
-  
-    useEffect(() => {
-      // cualquier efecto secundario aquí
-    }, []);
-  
+const SimpleSlider = ({images}) => {
+  console.log(images)
+  const CustomPrevArrow = (props) => {
+    const { className, style, onClick } = props;
     return (
-      <div style={{alignItems:"center",width:"730px",height:"384px",marginLeft:"300px"}} >
-        <Slider {...settings}>
-          <div>
-            <img src={house1}/> 
-          </div>
-          <div>
-            <img src={house2}/>
-          </div>
-          <div>
-            <img src={house3}/>
-          </div>
-        </Slider>
+      <div
+        className={className}
+        style={{ ...style,background:"gray",height:"0px",width:"0px"}}
+        onClick={onClick}
+      >
+        {Icons.arrowLeft}
       </div>
     );
-  }
+  };
+    
+  const CustomNextArrow = (props) => {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style,}}
+        onClick={onClick}>
+        {Icons.arrowRight}
+      </div>
+    );
+    };
+  const [settings, setSettings] = useState({
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    arrows:true,
+    prevArrow: <CustomPrevArrow style={{height:20}} />,
+    nextArrow: <CustomNextArrow />,
+  });
+  
+  return (
+    <div style={{width:"100%"}} >
+      <Slider {...settings}>
+        {images.map(image=>{
+          return(
+            <div>
+              <img src={image} style={{height:500}} alt={"Sin imagen"}/> 
+            </div>    
+          )
+        })}
+        
+      </Slider>
+    </div>
+  );
+}
   
   export default SimpleSlider;
