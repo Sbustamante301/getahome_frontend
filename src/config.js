efec44190ab9b17e04dc81ccb3204a1388be997e
Jesus Barboza <jbarz20@gmail.com>
2023-02-08 15:27:13 -0500

    CHORE: set the production's backend

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const tokenKey = "react_getaHome";
 
-// export const BASE_URI = "https://getahomebackendv02-rest-api.up.railway.app/";
-export const BASE_URI = "http://127.0.0.1:3000/"
+export const BASE_URI = "https://getahomebackendv02-production.up.railway.app";
+// export const BASE_URI = "http://127.0.0.1:3000/"
 export const API_KEY = 'AIzaSyBigQIyKfrPa9-UoeYvchynDNdf2x6ySkA'
 
 
